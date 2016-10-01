78cac941d72912422d48482eb13bc4fb695a3d03
wertrain <wertrain@gmail.com>
2016-10-01 12:45:31 +0900

    add parser file

---
+++ CCI/Include/parser.h
@@ -0,0 +1,12 @@
+#ifndef CCI_PARSER_H_
+#define CCI_PARSER_H_
+
+namespace cci {
+namespace parser {
+
+int compile(const char* text, const int text_size);
+
+} // namespace parser
+} // namespace cci
+
+#endif // CCI_PARSER_H_
---
+++ CCI/Source/parser.cpp
@@ -0,0 +1,12 @@
+#include "../include/parser.h"
+
+namespace cci {
+namespace parser {
+
+int compile(const char* text, const int text_size)
+{
+    return 0;
+}
+
+} // namespace parser
+} // namespace cci
