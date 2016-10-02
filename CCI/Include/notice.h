e07bb73ae312c6d98636f3a2632385c22d792c61
wertrain <wertrain@gmail.com>
2016-10-02 17:01:07 +0900

    add notice file

---
+++ CCI/Include/notice.h
@@ -0,0 +1,19 @@
+#ifndef CCI_NOTICE_H_
+#define CCI_NOTICE_H_
+
+namespace cci {
+namespace notice {
+
+const int kMaxError = 256;
+
+struct Error
+{
+
+};
+
+bool Initialize();
+
+} // namespace notice
+} // namespace cci
+
+#endif  // CCI_NOTICE_H_
---
+++ CCI/Source/notice.cpp
@@ -0,0 +1,19 @@
+#include "../Include/notice.h"
+
+#include <iostream>
+#include <cstring>
+
+namespace cci {
+namespace notice {
+
+/**
+ * ƒGƒ‰[ŠÖ˜A‚Ì‰Šú‰»
+ */
+bool Initialize()
+{
+
+}
+
+
+} // namespace notice
+} // namespace cci
