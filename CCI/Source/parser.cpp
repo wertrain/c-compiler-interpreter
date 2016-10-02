cd602ac4ebbe2a4e4ce172c9eabb92fa6d5bf614
wertrain <wertrain@gmail.com>
2016-10-02 18:18:18 +0900

    fix warning

--- CCI/Source/parser.cpp
+++ CCI/Source/parser.cpp
@@ -1,10 +1,27 @@
 #include "../include/parser.h"
 
+#include <iostream>
+#include "../include/token.h"
+#include "../include/notice.h"
+
 namespace cci {
 namespace parser {
 
-int compile(const char* text, const int text_size)
+int compile(const char* name, const char* text, const int text_size)
 {
+    cci::notice::Initialize();
+
+    cci::token::Initialize(name, text, text_size);
+    cci::token::Token token;
+    while(cci::token::GetNext(token) && token.kind_ != cci::token::kEof)
+    {
+        switch (token.kind_)
+        {
+        case cci::token::kDigit:
+            break;
+        }
+    }
+
     return 0;
 }
 
