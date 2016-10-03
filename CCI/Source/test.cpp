79c8d7ff28bbfa4c16a7238cb9a8e7c4ff57b5b9
wertrain <wertrain@gmail.com>
2016-10-03 21:24:36 +0900

    update token test

--- CCI/Source/test.cpp
+++ CCI/Source/test.cpp
@@ -10,28 +10,42 @@ namespace test {
 
 bool TestToken()
 {
-    const char* sample_text = 
+    const char* sample_text[] = 
     {
-        "if (dt <= size) dt = 100;\n\
-        else dt = \'AB\';\n\
-        puts(\"abcd);\n"
+        {
+            "if (dt <= size) dt = 100;\n\
+            else dt = \'A\';\n\
+            puts(\"abcd\");\n"
+        },
+        {
+            "if (dt <= size) dt = 100;\n\
+            else dt = \'AB\';\n\
+            puts(\"abcd);\n"
+        }
     };
-
-    cci::notice::Initialize();
-
-    cci::token::Initialize("sample_text", sample_text, strlen(sample_text));
-    
-    cci::token::Token token;
-    cci::token::GetNext(token);
-    while (token.kind_ != cci::token::kEof)
+    for (int i = 0; i < 2; ++i)
     {
-        std::cout << token.text_ << " " << token.kind_ << " " << token.value_ << std::endl;
+        cci::notice::Initialize();
+        cci::token::Initialize("sample_text", sample_text[i], strlen(sample_text[i]));
+        cci::token::Token token;
         cci::token::GetNext(token);
-    }
-
-    if (cci::notice::GetNoticeCount() > 0)
-    {
-        cci::notice::PrintNotice();
+        
+        std::cout << "========================" << std::endl;
+        std::cout << "Test:" << i << std::endl;
+        while (token.kind_ != cci::token::kEof)
+        {
+            std::cout << token.text_ << " " << token.kind_ << " " << token.value_ << std::endl;
+            cci::token::GetNext(token);
+        }
+        if (cci::notice::GetNoticeCount() > 0)
+        {
+            cci::notice::PrintNotice();
+        }
+        else
+        {
+            std::cout << "no error." << std::endl;
+        }
+        std::cout << "========================" << std::endl;
     }
 
     return true;
