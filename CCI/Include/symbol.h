16dc5eacf8a643404d30794e903e34f573fcb7f4
wertrain <wertrain@gmail.com>
2016-10-09 21:33:25 +0900

    fix

--- CCI/Include/symbol.h
+++ CCI/Include/symbol.h
@@ -25,10 +25,10 @@ struct SymbolData
     char* name_;        /** 変数や関数の名前 */
     SymbolKind kind_;   /** 種類 */
     DataType dataType_; /** タイプ */
-    int arrayLength;    /** 配列長, 0 なら単純変数 */
-    char level;         /** 記憶領域。グローバルなら0, ローカルなら1 */
-    char args;          /** 関数の場合の引数個数 */
-    int address;        /** 変数のアドレス */
+    int arrayLength_;   /** 配列長, 0 なら単純変数 */
+    char level_;        /** 記憶領域, グローバルなら0, ローカルなら1 */
+    char args_;         /** 関数の場合の引数個数 */
+    int address_;       /** 変数のアドレス */
 };
 
 } // namespace symbol
